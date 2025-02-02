a39952c0638688c3f7f90871f52ef9755a81c565
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2025-02-02 17:16:03 +0700

    rename main

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Persma</title>
+    <title>Media Explant</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="{{ asset('css/scrollbar.css') }}">
     <link rel="shortcut icon" href="{{ asset('assets/dev-64.png') }}" type="image/png">
