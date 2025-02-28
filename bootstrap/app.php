7d5243de2994e2a54b5dce897c59971949456674
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2025-03-01 04:13:07 +0700

    Modified app middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use Laravel\Sanctum\Http\Middleware\CheckAbilities;
+use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,3 +19,14 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+    $app->withMiddleware([
+        // Middleware lainnya...
+        'auth:sanctum' => EnsureFrontendRequestsAreStateful::class,
+    ]);
+
+    $app->routeMiddleware([
+        // Middleware lainnya...
+        'abilities' => CheckAbilities::class,
+        'ability' => CheckForAnyAbility::class,
+    ]);
