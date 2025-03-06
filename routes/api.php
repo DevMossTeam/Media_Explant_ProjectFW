a7216fee91dc87bc422467dd983d748f0a78c1ed
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2025-03-06 08:52:48 +0700

    DVF11T10

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AuthSanctumMiddleware;
 use App\Http\Controllers\Api\Auth\{
     CreatePasswordController,
     ChangePasswordController,
@@ -8,7 +10,21 @@ use App\Http\Controllers\Api\Auth\{
     ForgotPasswordController,
     LogoutController
 };
-use App\Http\Middleware\AuthSanctumMiddleware;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 // Group route untuk autentikasi
 Route::prefix('auth')->group(function () {
@@ -23,3 +39,4 @@ Route::prefix('auth')->group(function () {
 Route::middleware([AuthSanctumMiddleware::class])->group(function () {
     Route::post('/logout', [LogoutController::class, 'logout']);
 });
+
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,11 @@ use App\Http\Controllers\Author\KaryaController;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
 */
 
 // Route untuk halaman utama (Beranda)
