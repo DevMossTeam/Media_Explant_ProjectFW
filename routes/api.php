cc456cce92293eab202cd7c195e93ca24ac33ea9
Cursed Pakel <danieldna1411@gmail.com>
2025-03-22 23:22:45 +0700

    Create API Sign Up

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\SignInController;
 use App\Http\Controllers\API\SignUpController;
 
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| Di sini Anda dapat mendaftarkan route API untuk aplikasi. Semua route
+| akan dimuat melalui RouteServiceProvider dan diberikan grup middleware "api".
 |
 */
 
@@ -21,9 +19,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Authentikasi
 Route::post('/login', [SignInController::class, 'login']);
 Route::post('/logout', [SignInController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::post('/register', [SignUpController::class, 'register']);
+// Pendaftaran Multi-Step
+// Step 1: Input data dasar & pengiriman OTP
+Route::post('/register-step1', [SignUpController::class, 'registerStep1']);
+// Step 2: Verifikasi OTP
+Route::post('/verify-otp', [SignUpController::class, 'verifyOtp']);
+// Step 3: Input password & pendaftaran akhir
+Route::post('/register-step3', [SignUpController::class, 'registerStep3']);
+
+// Endpoint tambahan untuk mendapatkan data user
 Route::get('/user', [SignUpController::class, 'getUsers']);
-Route::get('/user/{uid}', [SignUpController::class, 'getUserByUid']);
+Route::get('/user/{uid}', [SignUpController::class, 'getUserByUid']);   
