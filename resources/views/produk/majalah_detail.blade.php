418a59d0dcbce3026735e88c267132475b2d12a1
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2025-04-03 21:57:28 +0700

    Fix majalah detail views memory exhausted

--- resources/views/produk/majalah_detail.blade.php
+++ resources/views/produk/majalah_detail.blade.php
@@ -6,7 +6,7 @@
     <p>{{ \Carbon\Carbon::parse($majalah->release_date)->translatedFormat('d M Y') }}</p>
 
     <!-- Elemen Flipbook -->
-    <div class="_df_custom" source="{{ route('majalah.pdf.preview', ['id' => $majalah->id]) }}" style="width:100%;height:500px;"></div>
+    <div class="_df_custom" source="{{ route('pdf.preview', ['id' => $majalah->id]) }}" style="width:100%;height:500px;"></div>
 
     <a href="{{ route('majalah.index') }}" class="btn btn-secondary mt-3">Kembali</a>
 </div>
@@ -15,13 +15,13 @@
 <link rel="stylesheet" href="{{ asset('dflip/css/dflip.min.css') }}">
 
 <!-- Include DearFlip JS -->
-<script src="{{ asset('dflip/js/libs/jquery.min.js') }}"></script>
+<script src="{{ asset('dflip/js/libs/jquery.min.js') }}"></script> <!-- Jika belum ada jQuery -->
 <script src="{{ asset('dflip/js/dflip.min.js') }}"></script>
 
 <script>
     document.addEventListener("DOMContentLoaded", function() {
         new DFLIP.Book({
-            source: "{{ route('majalah.pdf.preview', ['id' => $majalah->id]) }}",
+            source: "{{ route('pdf.preview', ['id' => $majalah->id]) }}",
             container: document.querySelector("._df_custom"),
         });
     });
